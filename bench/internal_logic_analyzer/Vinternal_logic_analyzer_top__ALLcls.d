b535265305d2e3e329aef58756b6007cd485596f
Phung Cheng Fei <phung@archlinux.domain.name>
2017-08-11 00:42:04 +0800

    Solved syntax errors

--- bench/internal_logic_analyzer/Vinternal_logic_analyzer_top__ALLcls.d
+++
@@ -1,7 +0,0 @@
-Vinternal_logic_analyzer_top__ALLcls.o: \
- Vinternal_logic_analyzer_top__ALLcls.cpp \
- Vinternal_logic_analyzer_top.cpp Vinternal_logic_analyzer_top.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilatedos.h \
- Vinternal_logic_analyzer_top__Syms.h
--- bench/internal_logic_analyzer/internal_logic_analyzer.d
+++
@@ -1,6 +0,0 @@
-internal_logic_analyzer.o: ../internal_logic_analyzer.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilatedos.h \
- Vinternal_logic_analyzer_top.h \
- /usr/share/verilator/include/verilated_vcd_c.h
